634331bb563a35396aad61a2c82ce3ab95a9e7b5
khalil Aboulouane <72363005+khalil1fs@users.noreply.github.com>
2024-01-04 22:38:57 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,14 @@
-def function() {
+def build() {
   echo 'Building the app'
+}
+
+def test() {
+  echo 'Testing the app'
+}
+
+def run() {
+  // We have the access to VERSION that exist in the file we imported
+  echo "Running the app ${VERSION}"
 }
 
 return this
